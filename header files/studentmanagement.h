315bc56d5cc0d6bc9f68bd25c0b8c590bc7c2c62
Mohammad Ali <alikhanak012345@gmail.com>
2025-11-11 16:01:42 +0500

    added save to json file functionality

--- header files/studentmanagement.h
+++ header files/studentmanagement.h
@@ -3,11 +3,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct Student {
-char roll_no[10];
-char name[50];
-char year_sem[20];
-float cgpa;
+#define SUBJECTS 6
+
+struct marks
+{
+    int itfa;
+    int cp;
+    int ap;
+    int pst;
+    int cag;
+    int fe;
+
+    int itfa_credithours;
+    int cp_credithours;
+    int ap_credithours;
+    int pst_credithours;
+    int cag_credithours;
+    int fe_credithours;
+
+    float itfa_gradescore;
+    float cp_gradescore;
+    float ap_gradescore;
+    float pst_gradescore;
+    float cag_gradescore;
+    float fe_gradescore;
+};
+
+struct Student
+{
+     char roll_no[10];
+    char name[50];
+    char semester[20];
+    float cgpa;
+    struct marks marks_info;
+    int attendance[SUBJECTS];
+
 };
 
 extern struct Student *students;
@@ -18,7 +48,7 @@ void displayStudents();
 void searchStudent();
 void editStudent();
 void deleteStudent();
-void saveToFile();
-void loadFromFile();
+void saveDataArray(struct Student *s, int cnt);
+int loadData(struct Student *students, int maxCount);
 void menu();
 #endif
